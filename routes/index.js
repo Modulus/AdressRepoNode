67e5d7070c0e3bac21e2cad6a062e59cb0ef875f
Modulus <thejohnskauge@gmail.com>
2016-07-14 22:17:43 +0200

    Updated, added back bin folder, this was missing ... :O

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,30 @@
 var express = require('express');
 var router = express.Router();
+var mongoClient = require('mongodb').MongoClient;
+
+var url = 'mongodb://localhost:27017/address';
+// Use connect method to connect to the Server
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+  res.json({message: "Welcome to the api, you bastard"})
+});
+
+router.get('/address/:text', function (req, res) {
+  var searchText = req.params['text'];
+  if(searchText){
+    MongoClient.connect(url, function (err, db) {
+      db.command({text: 'address', search: searchText }, function(err, cb){
+        console.log(cb.results);
+        res.json(cb.results);
+      });
+    });
+  }
+  else {
+    res.status(500).send({error: 'Please specify a search text!'});
+  }
+
 });
 
 module.exports = router;
