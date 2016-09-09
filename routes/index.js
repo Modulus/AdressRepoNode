5ed234b2fb0e7ad1204ce3c25bad08791a5431a0
Modulus <thejohnskauge@gmail.com>
2016-09-09 13:14:12 +0200

    Added get call for search

--- routes/index.js
+++ routes/index.js
@@ -22,6 +22,24 @@ router.get('/', function(req, res, next) {
   res.json({message: "Welcome to the api, you bastard"})
 });
 
+
+router.get('/search', function(req, res){
+  var searchText = req.query.text
+  console.log(searchText);
+  if(searchText){
+    console.log('Using searchText: '+searchText);
+    mongoClient.connect(url, function (err, db) {
+      var address = db.collection('address');
+      //console.log(address.find({}));
+      db.collection('address').find({$text: {$search: searchText, $caseSensitive: false}}).toArray(function (err, items) {
+        console.log('Items: ' + items);
+        res.json(items);
+      });
+    });
+
+  res.json(req.query.text)
+});
+
 router.post('/search', function (req, res) {
   var url = getConnectionString();
   console.log("Using connectionString: "+url);
